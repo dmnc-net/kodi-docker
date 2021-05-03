ebd6f84d10ff210a29521d906825730d5af91473
dmnc-net <lurid@dmnc.net>
2021-05-04 01:17:14 +0200

    moar variables :)

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+KODI_DATA="kodi-data"
 if [ "$1" == "" ]; then echo "* usage: $0 tag";false;exit;fi
 docker build --rm --build-arg UID=$UID -f Dockerfile.$1 -t kodi:$1 .
-if [ "$2" == "purge" ]; then docker volume rm kodi-data;fi
+if [ "$2" == "purge" ]; then docker volume rm $KODI_DATA;fi
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 xhost +local:docker > /dev/null
-docker volume inspect kodi-data >/dev/null 2>&1 || docker volume create kodi-data
-mkdir -p ~/.kodi/temp ~/.kodi/userdata
-touch ~/.kodi/temp/kodi.log
+mkdir -p $HOME/.kodi/temp $HOME/.kodi/userdata
+touch $HOME/.kodi/temp/kodi.log
 
 TAG=$1
 IMAGE="kodi:$TAG"
 NAME="Kodi-$TAG"
+KODI_DATA="kodi-data"
 CT="--rm"
 NETWORK="--network host"
 TZ=$(timedatectl|grep zone|awk '{print $3}')
@@ -15,7 +15,8 @@ PULSE="-v /run/user/$UID/pulse:/run/user/$UID/pulse -e PULSE_SERVER=unix:/run/us
 DEV="--device /dev/dri --device /dev/snd"
 DBUS="-v /run/dbus:/run/dbus -v /var/lib/dbus/machine-id:/var/lib/dbus/machine-id"
 V_HOMEFOLDER="-v $HOME:/home/kodi"
-V_KODI="-v kodi-data:/home/kodi/.kodi"
+V_KODI="-v $KODI_DATA:/home/kodi/.kodi"
 V_USERDATA="-v $HOME/.kodi/userdata:/home/kodi/.kodi/userdata"
 V_KODILOG="-v $HOME/.kodi/temp/kodi.log:/home/kodi/.kodi/temp/kodi.log"
+docker volume inspect $KODI_DATA >/dev/null 2>&1 || docker volume create $KODI_DATA
 docker run -ti $CT $NETWORK --name $NAME -e TZ=$TZ $DEV $PULSE $DBUS $X11 $V_HOMEFOLDER $V_KODI $V_USERDATA $V_KODILOG $IMAGE
